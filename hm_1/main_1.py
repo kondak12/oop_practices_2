940732057eac9294ca7677ae1c571da1a88277f7
Ivan <ivan.kondakov.98@bk.ru>
2025-06-08 12:13:01 +0300

    Task 2 is complete!

--- hm_1/main_1.py
+++ hm_1/main_1.py
@@ -20,3 +20,29 @@ class Animal:
         return f"{self.type} издаёт звук: '{sound}'."
 
 
+
+# Task 2
+
+class Book:
+
+    name: str
+    author: str
+    page_count: int
+
+    def __init__(self, name: str, author: str, page_count: int):
+        self.name = name
+        self.author = author
+        self.page_count = page_count
+
+
+    def __str__(self):
+        return f"Название книги: {self.name};\nАвтор: {self.author};\nКоличество страниц: {self.page_count}."
+
+
+    def open_page(self, page_number: int):
+        if (page_number > self.page_count) or (page_number < 1):
+            result = f"Такой страницы не существует."
+        else:
+            result = f"Страница №{page_number} открылась."
+
+        return result
